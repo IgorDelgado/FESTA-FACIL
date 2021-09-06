f69dffb6bb850fa7a32f4b498a43c4d29690712a
User <User@DESKTOP-7O84FIG>
2021-09-06 08:42:09 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FESTA_FACIL.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
